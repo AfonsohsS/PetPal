c3a6aaf0b754526f9b27246f7786e81b0c5845e8
AfonsohsS <afonsohs@gmail.com>
2019-06-12 06:37:38 -0300

    Initiated CloudKit implementation.

--- PetPal/AppDelegate.swift
+++ PetPal/AppDelegate.swift
@@ -72,24 +72,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "PetPal")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            print(storeDescription)
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Error Reading PersistentStore: \(error), \(error.userInfo)")
-            }
-        })
+        let container = NSPersistentCloudKitContainer(name: "PetPal")
+        
+        // get the store description
+        guard let description = container.persistentStoreDescriptions.first else {
+            fatalError("Could NOT retrieve a persistent store description")
+        }
+        
+        // initialize the CloudKit scheme
+        let id = "iCloud.com.example.afonsohs.PetPal"
+        let options = NSPersistentCloudKitContainerOptions(containerIdentifier: id)
+        options.shouldInitializeSchema = true // Toggle to false when done and send to Production mode
+        description.cloudKitContainerOptions = options
+        
+        
+//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+//            print(storeDescription)
+//            if let error = error as NSError? {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//
+//                /*
+//                 Typical reasons for an error here include:
+//                 * The parent directory does not exist, cannot be created, or disallows writing.
+//                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+//                 * The device is out of space.
+//                 * The store could not be migrated to the current model version.
+//                 Check the error message to determine what the actual problem was.
+//                 */
+//                fatalError("Error Reading PersistentStore: \(error), \(error.userInfo)")
+//            }
+//        })
         return container
     }()
     
